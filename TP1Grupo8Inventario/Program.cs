3378caacea4ff39fc7ee90098b8b1a24522949da
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-10-11 20:23:10 -0300

    avance grupal

--- TP1Grupo8Inventario/Program.cs
+++ TP1Grupo8Inventario/Program.cs
@@ -13,8 +13,42 @@ namespace TP1Grupo8Inventario
     {
         static void Main(string[] args)
         {
+            // declaremos variables globales
+            //comprobar si existe si existe le damos bienvenida y le ofrecemos un  "menú" : buscar, borrar, ingresar, editar.
+            //  luego de elegir la opcion del menu se debe poder acceder a los productos por un id.
 
-            
+            // crear el diccionario en la cual vamos tener los productos cargados.
+            // dentro de diccionario por medio de funciones tenemos que poder hacer las opciobuones que te ofrece el menú. " buscar
+            // borrar, ingresar, editar".
+
+            // crear la logica para informar si el producto existe o no existe, si hay sotck o no hay stock. 
+            Console.WriteLine("Ingrese su usuario: ");
+            string usuario;
+            int id;
+            usuario = Console.ReadLine();
+            Console.WriteLine("Bienvenido!");
+
+            switch ()
+            {
+
+                case valor1:
+                    Console.WriteLine("Buscar articulo");
+                case valor2:
+                    Console.WriteLine("articulo buscado");
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
         }
     }
 }
+ 
