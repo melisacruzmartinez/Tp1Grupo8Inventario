08fa9ecb16035fb0db78713c4ad964a19349fa6c
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-10-11 18:55:31 -0300

    First commit

--- TP1Grupo8Inventario/Program.cs
+++ TP1Grupo8Inventario/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Vamos a crear un inventario para restautantes utilizando Diccionarios con id diferente para cada articulo, con swich case crearemos una 
+// lista de opciones para elegir entre borrar un producto, cargar un producto y revisar stock. Para cada una de opciones crearemos una  
+// funcion diferente. 
 namespace TP1Grupo8Inventario
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            
         }
     }
 }
